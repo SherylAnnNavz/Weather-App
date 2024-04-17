d60677a35169cc154650d0a749ea07a837d3fe32
SherylAnnNavz <sherylnavarro29@gmail.com>
2024-04-17 17:04:08 +0800

    Added api

--- weather/weather_site/main/views.py
+++ weather/weather_site/main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+import urllib.request
+import json
 
 # Create your views here.
 def index(request):
-    data = {}
+    if request.method == 'POST':
+        city = 'mexico'
+        source = urllib.request.urlopen('http://api.openweathermap.org/data2.5/weather?q='+city+'appid=770a17f9520e41124656aa601bc34b3c').read()
+        list_of_data = json.loads(source)
+    else:
+        data = {}
     return render(request, 'main/index.html', data)
