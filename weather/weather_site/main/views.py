f230b70c3ebbb7295d8c26261cb7d88901350dea
SherylAnnNavz <sherylnavarro29@gmail.com>
2024-04-17 17:07:39 +0800

    Made some modifications

--- weather/weather_site/main/views.py
+++ weather/weather_site/main/views.py
@@ -5,9 +5,11 @@ import json
 # Create your views here.
 def index(request):
     if request.method == 'POST':
+        pass
+    else:
         city = 'mexico'
         source = urllib.request.urlopen('http://api.openweathermap.org/data2.5/weather?q='+city+'appid=770a17f9520e41124656aa601bc34b3c').read()
         list_of_data = json.loads(source)
-    else:
+        print(list_of_data)
         data = {}
     return render(request, 'main/index.html', data)
